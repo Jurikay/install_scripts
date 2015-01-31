29bbcb07bf36228a4072a3eacef49d838981a4b8
Jurikay <jurek.baumann@gmail.com>
2015-01-31 01:58:42 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,26 +12,6 @@ read starten
 
 sleep 1
 
-# Check XCode command line tools
-function checkXCode() {
-    if [[ ! -x /usr/bin/gcc ]]; then
-        echob "ERROR:"
-        echob "      Xcode Command Line Tools from Apple not found!"
-        echob "      CloverGrowerPro.sh needs it";echo
-        echob "      Going To Apple Developer Site"
-        echob "      Download & Install XCode Command Line Tools then re-run CloverGrowerPro.sh"
-        echo
-        echob "      Press enter to open a browser to download XCode Command Line Tools"
-        read
-        open "https://developer.apple.com/downloads/"
-        wait
-        tput bel
-        exit 0
-    fi
-}
-
-
-
 git clone https://github.com/Jurikay/juridotfiles.git $HOME/dev
 
 sleep 1
