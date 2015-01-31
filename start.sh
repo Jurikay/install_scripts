7a5b4d8cf31b8ab380830985d07f4a99a36ba833
Jurikay <unconfigured@null.spigotmc.org>
2015-01-31 01:50:26 +0100

    push

--- start.sh
+++ start.sh
@@ -26,7 +26,7 @@ function checkXCode() {
         open "https://developer.apple.com/downloads/"
         wait
         tput bel
-        exit 1
+        exit 0
     fi
 }
 
@@ -37,23 +37,3 @@ git clone https://github.com/Jurikay/juridotfiles.git $HOME/dev
 sleep 1
 
 bash $HOME/dev/scripts/installer
-
-
-
-# Check XCode command line tools
-function checkXCode() {
-    if [[ ! -x /usr/bin/gcc ]]; then
-        echob "ERROR:"
-        echob "      Xcode Command Line Tools from Apple not found!"
-        echob "      CloverGrowerPro.sh needs it";echo
-        echob "      Going To Apple Developer Site"
-        echob "      Download & Install XCode Command Line Tools then re-run CloverGrowerPro.sh"
-        echo
-        echob "      Press enter to open a browser to download XCode Command Line Tools"
-        read
-        open "https://developer.apple.com/downloads/"
-        wait
-        tput bel
-        exit 1
-    fi
-}
